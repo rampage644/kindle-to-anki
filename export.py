c882aa222574bebfaba905f1daeaa9aa8e8de0bf
Sergei Turukin <rampage644@gmail.com>
2014-11-24 14:59:22 +0200

    Fix TIMESTAMP file location

--- export.py
+++ export.py
@@ -13,7 +13,7 @@ import urllib
 import urllib.parse
 import urllib.request
 
-TIMESTAMP_PATH = '/home/ramp/.kindle'
+TIMESTAMP_PATH = os.path.expanduser('~/.kinlde')
 
 def get_lookups(db, timestamp=0):
     conn = sqlite3.connect(db)
